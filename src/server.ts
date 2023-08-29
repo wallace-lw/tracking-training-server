32638a539075fbd6526cdc7f007d3ca928917675
WallaceLeonhard <wallacelw22@gmail.com>
2023-08-29 11:32:35 -0300

    chore: removing unused code

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import Express from 'express'
+import { CreateUserController } from './controllers/CreateUserController'
+import { prisma } from './database'
 
 const app = Express()
-
 const port = 5000
 
-app.use(Express.json())
+const createUserController = new CreateUserController()
 
-app.get('/', (request, response) => response.status(200).send('Hello World!'))
+app.get('/', (request, response) =>
+  response.status(200).send({ message: 'Oi' }),
+)
+
+app.post('/create-user', createUserController.create)
+
+app.get('/users')
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
