5924912c090c743d9f44d2fa01c98e7c16bf3d02
WallaceLeonhard <wallacelw22@gmail.com>
2023-09-09 02:57:00 -0300

    feat: adding the workout routes to use

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { userRoutes } from './user.routes'
+import { workoutRoutes } from './workout.routes'
 
 const router = Router()
 
 router.use('/users', userRoutes)
+router.use('/workouts', workoutRoutes)
 
 export { router }
