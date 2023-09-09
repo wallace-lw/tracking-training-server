5efb924b66d22710a29d9e4ab131c44b9fcabeb1
WallaceLeonhard <wallacelw22@gmail.com>
2023-09-09 02:57:43 -0300

    feat: adding the create route to the workout router

--- src/routes/workout.routes.ts
+++ src/routes/workout.routes.ts
@@ -1,3 +1,8 @@
+import { CreateWorkoutController } from '@/controllers/workout/create-workout-controller'
 import { Router } from 'express'
 
 export const workoutRoutes = Router()
+
+const createWorkoutController = new CreateWorkoutController()
+
+workoutRoutes.post('/', createWorkoutController.handle)
