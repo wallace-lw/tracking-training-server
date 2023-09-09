02c3e92e526f09bcd5672bf9be0cff736ef066f1
WallaceLeonhard <wallacelw22@gmail.com>
2023-09-09 20:36:40 -0300

    feat: adding the find workout by id route

--- src/routes/workout.routes.ts
+++ src/routes/workout.routes.ts
@@ -1,8 +1,11 @@
+import { FindWorkoutByIdController } from '@/controllers/workout'
 import { CreateWorkoutController } from '@/controllers/workout/create-workout-controller'
 import { Router } from 'express'
 
 export const workoutRoutes = Router()
 
 const createWorkoutController = new CreateWorkoutController()
+const findWorkoutById = new FindWorkoutByIdController()
 
 workoutRoutes.post('/', createWorkoutController.handle)
+workoutRoutes.get('/:id', findWorkoutById.handle)
