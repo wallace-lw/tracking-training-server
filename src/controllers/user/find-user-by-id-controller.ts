18662d56800465f106c8c20544f538c236dc830f
wallace-lw <wallacelw22@gmail.com>
2023-09-11 19:47:39 -0300

    refactor: using try cacth to handle errors

--- src/controllers/user/find-user-by-id-controller.ts
+++ src/controllers/user/find-user-by-id-controller.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from 'express'
 import { FindUserByIdService } from '@/services/user'
+import { AppError } from '@/errors/app-errors'
 
 export class FindUserbyIdController {
   async handle(request: Request, response: Response) {
-    const findUserById = new FindUserByIdService()
-    const { id } = request.params
-    const user = await findUserById.gettingUser(id)
+    try {
+      const findUserById = new FindUserByIdService()
+      const { id } = request.params
+      if (!id) {
+        return response.status(400).send({
+          message: 'Id is required',
+        })
+      }
 
-    return response.status(200).send(user)
+      const user = await findUserById.gettingUser(id)
+      if (!user) return response.status(404).send({ message: 'User not found' })
+
+      return response.status(200).send(user)
+    } catch (error) {
+      if (error instanceof AppError) {
+        return response.status(error.statusCode).json({ error: error.message })
+      }
+      return response.status(500).json({ error: 'Internal server error' })
+    }
   }
 }
