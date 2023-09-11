1f0f905360757271d49f4559e0ff3ca91237e94d
wallace-lw <wallacelw22@gmail.com>
2023-09-11 19:45:48 -0300

    feat: creating a route for the find many workouts  http request

--- src/routes/workout.routes.ts
+++ src/routes/workout.routes.ts
@@ -1,11 +1,14 @@
 import { FindWorkoutByIdController } from '@/controllers/workout'
 import { CreateWorkoutController } from '@/controllers/workout/create-workout-controller'
+import { FindManyWorkoutsController } from '@/controllers/workout/find-many-workouts-controller'
 import { Router } from 'express'
 
 export const workoutRoutes = Router()
 
 const createWorkoutController = new CreateWorkoutController()
 const findWorkoutById = new FindWorkoutByIdController()
+const findWorkouts = new FindManyWorkoutsController()
 
 workoutRoutes.post('/', createWorkoutController.handle)
 workoutRoutes.get('/:id', findWorkoutById.handle)
+workoutRoutes.get('/', findWorkouts.handle)
