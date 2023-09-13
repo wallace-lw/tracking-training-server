358dbc9ab6dd2a6813b09fa3674840bb8773cd00
WallaceLeonhard <wallacelw22@gmail.com>
2023-09-13 15:54:38 -0300

    feat: adding a new field to the UserRepository interface

--- src/interfaces/user-repository.ts
+++ src/interfaces/user-repository.ts
@@ -1,5 +1,8 @@
 import { UserDTO } from '@/dtos/user-dto'
 
 export interface IUserRepository {
+  createUser(username: string, password: string): Promise<void>
+  // findUserByUsername(username: string): Promise<UserDTO>
   findUserById(id: string): Promise<UserDTO | null>
+  // findAllUsers(): Promise<UserDTO>
 }
