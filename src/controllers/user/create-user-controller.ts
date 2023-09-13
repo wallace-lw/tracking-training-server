e25ae7814289a958756ad10d7aafda56e941d5dc
WallaceLeonhard <wallacelw22@gmail.com>
2023-09-13 15:50:49 -0300

    refactor: refactoring the create user controller

--- src/controllers/user/create-user-controller.ts
+++ src/controllers/user/create-user-controller.ts
@@ -1,13 +1,21 @@
 import { Request, Response } from 'express'
 import { createUserSchema } from '@/utils/zodSchemas/createUserSchema'
 import { CreateUserService } from '@/services/user'
+import { AppError } from '@/errors/app-errors'
 
 export class CreateUserController {
   async handle(request: Request, response: Response) {
-    const createUserService = new CreateUserService()
-    const data = createUserSchema.parse(request.body)
-    await createUserService.create(data)
+    try {
+      const createUserService = new CreateUserService()
+      const { username, password } = createUserSchema.parse(request.body)
+      await createUserService.create(username, password)
 
-    return response.status(201).send('User created successfully!')
+      return response.status(201).send('User created successfully!')
+    } catch (error) {
+      if (error instanceof AppError) {
+        return response.status(error.statusCode).json({ error: error.message })
+      }
+      return response.status(500).json({ error: 'Internal Server Error' })
+    }
   }
 }
