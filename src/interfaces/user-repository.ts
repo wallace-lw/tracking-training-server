da6064b1d9c581a5897885161f4e5bc022a76908
wallace-lw <wallacelw22@gmail.com>
2023-09-14 18:41:52 -0300

    feat: adding types of the implementation to the create user method

--- src/interfaces/user-repository.ts
+++ src/interfaces/user-repository.ts
@@ -1,7 +1,8 @@
 import { UserDTO } from '@/dtos/user-dto'
+import { IUser } from './IUser'
 
 export interface IUserRepository {
-  createUser(username: string, password: string): Promise<void>
+  createUser({ username, password }: IUser): Promise<void>
   // findUserByUsername(username: string): Promise<UserDTO>
   findUserById(id: string): Promise<UserDTO | null>
   // findAllUsers(): Promise<UserDTO>
