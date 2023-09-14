5b72748e2f22e9dc065549624330bc36309f3c68
wallace-lw <wallacelw22@gmail.com>
2023-09-14 20:09:34 -0300

    fix: fixing the body the create user service

--- src/services/user/create-user-service.ts
+++ src/services/user/create-user-service.ts
@@ -1,15 +1,16 @@
 import { UserRepository } from '@/repositories/user-repository'
 import { AppError } from '@/errors/app-errors'
+import { IUser } from '@/interfaces'
 
 export class CreateUserService {
-  async create(username: string, password: string) {
+  async create(data: IUser) {
     const userRepo = new UserRepository()
-    const userExists = await userRepo.findUserByUsername(username)
+    const userExists = await userRepo.findUserByUsername(data.username)
 
     if (userExists) {
       throw new AppError('User already exists')
     }
 
-    await userRepo.createUser(username, password)
+    await userRepo.createUser(data)
   }
 }
