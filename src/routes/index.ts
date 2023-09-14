22c8a86e052a998789e15a561d7bfafedadf56aa
wallace-lw <wallacelw22@gmail.com>
2023-09-14 20:08:23 -0300

    feat: creating the exercise routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { userRoutes } from './user.routes'
 import { workoutRoutes } from './workout.routes'
+import { exercisesRoutes } from './exercise.routes'
 
 const router = Router()
 
 router.use('/users', userRoutes)
 router.use('/workouts', workoutRoutes)
+router.use('/exercises', exercisesRoutes)
 
 export { router }
