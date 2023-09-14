328db3bfedf8aa6c94efc6ea5bcb95529a98067d
wallace-lw <wallacelw22@gmail.com>
2023-09-14 20:49:37 -0300

    feat: new method on the UserRepository implementation

--- src/interfaces/user-repository.ts
+++ src/interfaces/user-repository.ts
@@ -4,6 +4,6 @@ import { IUser } from './IUser'
 export interface IUserRepository {
   createUser({ username, password }: IUser): Promise<void>
   // findUserByUsername(username: string): Promise<UserDTO>
-  findUserById(id: string): Promise<UserDTO | null>
+  findUserById({ id }: IUser): Promise<UserDTO | null>
   // findAllUsers(): Promise<UserDTO>
 }
