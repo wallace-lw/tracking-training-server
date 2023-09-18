74de55b0bda5997cdf486bfae901f3754839db36
wallace-lw <wallacelw22@gmail.com>
2023-09-18 17:07:10 -0300

    feat: creating the remains methods of the exercise repository

--- src/repositories/exercise-repository.ts
+++ src/repositories/exercise-repository.ts
@@ -1,14 +1,49 @@
 import { prisma } from '@/database'
 import { IExercise } from '@/interfaces'
-import { IExerciseRepository } from '@/interfaces/exercise-repository'
+import { IExerciseImplementation } from '@/implementations/'
 
-export class ExerciseRepository implements IExerciseRepository {
+export class ExerciseRepository implements IExerciseImplementation {
   async createExercise({ name, reps, workoutId }: IExercise): Promise<void> {
     await prisma.exercise.create({
       data: {
         name,
         reps,
         workoutId,
+      },
+    })
+  }
+
+  async getExercise({ id }: IExercise): Promise<IExercise> {
+    const response = await prisma.exercise.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return response as IExercise
+  }
+
+  async getallExercises(): Promise<IExercise[]> {
+    const response = await prisma.exercise.findMany()
+    return response as IExercise[]
+  }
+
+  async updateExercise({ id, name, reps }: IExercise): Promise<void> {
+    await prisma.exercise.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+        reps,
+      },
+    })
+  }
+
+  async deleteExercise({ id }: IExercise): Promise<void> {
+    await prisma.exercise.delete({
+      where: {
+        id,
       },
     })
   }
