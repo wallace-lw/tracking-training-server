cf60d42d1e5cee3df09573673f8c9bd8352644b8
wallace-lw <wallacelw22@gmail.com>
2023-09-18 17:05:52 -0300

    refactor: changing the names of the implementations

--- src/interfaces/exercise-repository.ts
+++
@@ -1,5 +0,0 @@
-import { IExercise } from './IExercise'
-
-export interface IExerciseRepository {
-  createExercise({ name, reps, workoutId }: IExercise): Promise<void>
-}
--- src/interfaces/user-repository.ts
+++
@@ -1,9 +0,0 @@
-import { UserDTO } from '@/dtos/user-dto'
-import { IUser } from './IUser'
-
-export interface IUserRepository {
-  createUser({ username, password }: IUser): Promise<void>
-  // findUserByUsername(username: string): Promise<UserDTO>
-  findUserById({ id }: IUser): Promise<UserDTO | null>
-  // findAllUsers(): Promise<UserDTO>
-}
