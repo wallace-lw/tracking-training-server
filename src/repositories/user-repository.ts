258ad8124312f0e5888bb74a9e7ddd0e0ac40475
wallace-lw <wallacelw22@gmail.com>
2023-09-18 22:35:21 -0300

    refactor: updating the method in the user repository

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,10 +1,10 @@
-import { IUser } from '../interfaces'
 import { prisma } from '../database'
 import { UserDTO } from '@/dtos/user-dto'
 import { IUserImplementation } from '@/implementations'
+import { IUser } from '@/interfaces'
 
 export class UserRepository implements IUserImplementation {
-  async createUser({ username, password }: IUser): Promise<void> {
+  async createUser({ username, password }: Omit<IUser, 'id'>): Promise<void> {
     await prisma.user.create({
       data: {
         username,
@@ -13,25 +13,20 @@ export class UserRepository implements IUserImplementation {
     })
   }
 
-  async findUserByUsername(username: string) {
-    return await prisma.user.findUnique({
+  async findUserByUsername(username: string): Promise<UserDTO> {
+    const user = await prisma.user.findUnique({
       where: {
         username,
       },
     })
+    return user as unknown as UserDTO
   }
 
-  async findUserById({ id }: IUser): Promise<UserDTO> {
+  async findUserById(id: string): Promise<UserDTO> {
     const response = await prisma.user.findUnique({
       where: {
         id,
       },
-    })
-    return response as UserDTO
-  }
-
-  async findAllUsers() {
-    return await prisma.user.findMany({
       select: {
         id: true,
         username: true,
@@ -41,9 +36,31 @@ export class UserRepository implements IUserImplementation {
             title: true,
             type: true,
             exercises: true,
+            userId: true,
           },
         },
       },
     })
+    return response as UserDTO
+  }
+
+  async findAllUsers(): Promise<UserDTO[]> {
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        username: true,
+        workouts: {
+          select: {
+            id: true,
+            title: true,
+            type: true,
+            exercises: true,
+            userId: true,
+          },
+        },
+      },
+    })
+
+    return users as UserDTO[]
   }
 }
