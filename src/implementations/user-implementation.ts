6e4547564067c64b529b9cba5035e741fc31b111
wallace-lw <wallacelw22@gmail.com>
2023-09-18 22:33:01 -0300

    feat: creating the user implementation interface

---
+++ src/implementations/user-implementation.ts
@@ -0,0 +1,9 @@
+import { UserDTO } from '@/dtos/user-dto'
+import { IUser } from '@/interfaces'
+
+export interface IUserImplementation {
+  createUser({ username, password }: IUser): Promise<void>
+  findUserByUsername(username: string): Promise<UserDTO>
+  findUserById(id: string): Promise<UserDTO>
+  findAllUsers(): Promise<UserDTO[]>
+}
