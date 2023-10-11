049b7e9e15cefd6fda35a0a84bf87e79c71a4e64
wallace-lw <wallacelw22@gmail.com>
2023-10-11 20:16:58 -0300

    feat: creating the delete workout route

--- src/routes/workout.routes.ts
+++ src/routes/workout.routes.ts
@@ -3,6 +3,7 @@ import { FindWorkoutByIdController } from '@/controllers/workout'
 import { CreateWorkoutController } from '@/controllers/workout/create-workout-controller'
 import { FindManyWorkoutsController } from '@/controllers/workout/find-many-workouts-controller'
 import { UpdateWorkoutController } from '@/controllers/workout/update-workout-controller'
+import { DeleteWorkoutController } from '@/controllers/workout/delete-workout-controller'
 
 export const workoutRoutes = Router()
 
@@ -10,8 +11,10 @@ const createWorkoutController = new CreateWorkoutController()
 const findWorkoutById = new FindWorkoutByIdController()
 const findWorkouts = new FindManyWorkoutsController()
 const updateWorkout = new UpdateWorkoutController()
+const deleteWorkout = new DeleteWorkoutController()
 
 workoutRoutes.post('/', createWorkoutController.handle)
 workoutRoutes.get('/:id', findWorkoutById.handle)
 workoutRoutes.get('/', findWorkouts.handle)
 workoutRoutes.put('/:id', updateWorkout.handle)
+workoutRoutes.delete('/:id', deleteWorkout.handle)
