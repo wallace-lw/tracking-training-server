e97d6125f53fe122f7b13f4d3d278a795cfd68b4
wallace-lw <wallacelw22@gmail.com>
2023-10-11 20:00:01 -0300

    Revert "feat: adding the initial error handlers"
    This reverts commit bba7ed42423ef137947aae1880655574b4bdb697.

--- src/controllers/workout/find-many-workouts-controller.ts
+++ src/controllers/workout/find-many-workouts-controller.ts
@@ -4,9 +4,7 @@ import { Request, Response } from 'express'
 export class FindManyWorkoutsController {
   async handle(request: Request, response: Response) {
     const workout = new FindManyWorkoutsService()
-    const { userId } = request.params
-
-    const workouts = await workout.find(userId)
+    const workouts = await workout.find()
 
     return response.status(200).json(workouts)
   }
