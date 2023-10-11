3ef76516aa50ea0045d667ec7a9ff6607d97c2b9
wallace-lw <wallacelw22@gmail.com>
2023-10-11 19:40:40 -0300

    feat: creating route for the update workout feature

--- src/routes/workout.routes.ts
+++ src/routes/workout.routes.ts
@@ -1,14 +1,17 @@
+import { Router } from 'express'
 import { FindWorkoutByIdController } from '@/controllers/workout'
 import { CreateWorkoutController } from '@/controllers/workout/create-workout-controller'
 import { FindManyWorkoutsController } from '@/controllers/workout/find-many-workouts-controller'
-import { Router } from 'express'
+import { UpdateWorkoutController } from '@/controllers/workout/update-workout-controller'
 
 export const workoutRoutes = Router()
 
 const createWorkoutController = new CreateWorkoutController()
 const findWorkoutById = new FindWorkoutByIdController()
 const findWorkouts = new FindManyWorkoutsController()
+const updateWorkout = new UpdateWorkoutController()
 
 workoutRoutes.post('/', createWorkoutController.handle)
 workoutRoutes.get('/:id', findWorkoutById.handle)
 workoutRoutes.get('/', findWorkouts.handle)
+workoutRoutes.put('/:id', updateWorkout.handle)
